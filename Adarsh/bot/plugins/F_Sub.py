c23ceade8d8447bfe7e92099ced031e51835e10c
MOVIESWORLDBoy <117733454+MOVIESWORLDBoy@users.noreply.github.com>
2023-04-28 18:48:28 +0530

    Update F_Sub.py

--- Adarsh/bot/plugins/F_Sub.py
+++ Adarsh/bot/plugins/F_Sub.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 import datetime
-from ..vars import vars
+from vars import var
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied
@@ -11,31 +11,31 @@ from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOc
 
 @Client.on_message(filters.private & filters.incoming)
 async def force_sub(c, m):
-    if Config.FORCE_SUB:
+    if var.FORCE_SUB:
         try:
-            chat = await c.get_chat_member(Config.FORCE_SUB, m.from_user.id)
+            chat = await c.get_chat_member(var.FORCE_SUB, m.from_user.id)
             if chat.status=='kicked':
                 return await m.reply_text('Hai Brooh!You Where Banned🤭Kicked From Server Contact Admin🤧',  quote=True)
 
         except UserNotParticipant:
-            button = [[InlineKeyboardButton('🔥 𝙹𝙾𝙸𝙽 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🔥', url=f'https://t.me/{Config.FORCE_SUB}')]]
+            button = [[InlineKeyboardButton('🔥 𝙹𝙾𝙸𝙽 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🔥', url=f'https://t.me/{var.FORCE_SUB}')]]
             markup = InlineKeyboardMarkup(button)
             return await m.reply_text(text="Hai Brooh!Join My Main Channel🤣Then Use....\n 🤭ഡെയ് പാക്കര ചാനലിൽ Join ചെയ്തിട്ട് ബാക്കി പണിയ്", parse_mode='markdown', reply_markup=markup, quote=True)
 
         except ChatAdminRequired:
-            logger.warning(f"Make me admin in @{Config.FORCE_SUB}")
-            if m.from_user.id in Config.AUTH_USERS:
-                return await m.reply_text(f"Make me admin in @{Config.FORCE_SUB}")
+            logger.warning(f"Make me admin in @{var.FORCE_SUB}")
+            if m.from_user.id in var.AUTH_USERS:
+                return await m.reply_text(f"Make me admin in @{var.FORCE_SUB}")
 
         except UsernameNotOccupied:
             logger.warning("The forcesub username was Incorrect. Please give the correct username.")
-            if m.from_user.id in Config.AUTH_USERS:
+            if m.from_user.id in var.AUTH_USERS:
                 return await m.reply_text("The forcesub username was Incorrect. Please give the correct username.")
 
         except Exception as e:
             if "belongs to a user" in str(e):
                 logger.warning("Forcesub username must be a channel username Not yours or any other users username")
-                if m.from_user.id in Config.AUTH_USERS:
+                if m.from_user.id in var.AUTH_USERS:
                     return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
             logger.error(e)
             return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [Channel](https://t.me/MW_BOTS)", disable_web_page_preview=True, quote=True)
